dc0da7dd6f0c2c01701e0d572fa2c48b548c2c4d
Roma Dergach <dergachroma@gmail.com>
2022-10-15 11:57:49 +0300

    feat: animations on slideOne

---
+++ src/components/Main/Main.js
@@ -0,0 +1,12 @@
+import "./Main.css"
+
+import React from "react";
+import SlideOne from "../SlideOne.js/SlideOne";
+
+const Main = () => {
+  return <div className="main">
+    <SlideOne />
+  </div>;
+};
+
+export default Main;
---
+++ src/components/SlideOne.js/SlideOne.js
@@ -0,0 +1,33 @@
+import React from "react";
+import "./SlideOne.css";
+
+const SlideOne = () => {
+  return (
+    <div className="slide slide-one">
+      <div className="slide-one__back-img slide-one__back-img_type_blueLong"></div>
+      <div className="slide-one__back-img slide-one__back-img_type_sperm"></div>
+      <div className="slide-one__back-img slide-one__back-img_type_spermBlur"></div>
+      <div className="slide-one__back-img slide-one__back-img_type_blurOne"></div>
+      <div className="slide-one__back-img slide-one__back-img_type_blurTwo"></div>
+
+      <div className="slide-one__back-img slide-one__back-img_type_blueBig"></div>
+      <div className="slide-one__back-img slide-one__back-img_type_blueCircleOne"></div>
+      <div className="slide-one__back-img slide-one__back-img_type_blueCircleTwo"></div>
+      <div className="slide-one__back-img slide-one__back-img_type_blueSpiny"></div>
+
+      <h1 className="slide-one__title">Привет,</h1>
+      <div className="slide-one__subtitle-wrapper">
+        <p className="slide-one__subtitle">
+          это <b className="slide-one__subtitle_style_bold">не</b> коммерческое
+          задание
+        </p>
+        <button className="slide-one__next-btn">
+          <div className="slide-one__arrow-icon"></div>
+          Что дальше?
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default SlideOne;
